5d4812d58af9491657ff4bc9d24e2031ec5ae584
Shriverdhan Pathak <emerginggiant18@gmail.com>
2023-07-30 15:19:10 +0530

    second last changes

--- revision/src/App.jsx
+++ revision/src/App.jsx
@@ -1,16 +1,98 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import Footer from './Footer'
 import './App.css'
 import Header from './Header'
+import AddItem from './AddItem'
 import Content from './Content'
+import SearchItem from './SearchItem'
+
 function App() {
-  const [count, setCount] = useState(0)
+const API_URL = 'http://localhost:3500/items'
+
+const [items,setItems] = useState([])
+const [newItem,setNewItem] = useState('')
+const [search,setSearch] = useState('')
+const [fetchError, setFetchError] = useState(null)
+const [isLoading, setIsLoading] = useState(true)
+
+useEffect(()=> {
+  
+ const fetchItems = async()=>{
+  try {
+    const response = await fetch(API_URL)
+    if(!response.ok) throw Error('did not receive expected data')
+    const listItems = await response.json()
+    
+    setItems(listItems)
+    setFetchError(null)
+  } catch (err) {
+    
+    setFetchError(err.message)
+  } finally {
+    setIsLoading(false)
+  }
+ }
+ setTimeout(()=>{
+  fetchItems()
+ },2000)
+ 
+},[])
+
+
+const addItem = (item) => {
+  const id = items.length ? items[items.length-1].id + 1 : 1
+  const myNewItem = { id, checked:false, item}
+  const listItems = [...items, myNewItem]
+  setItems(listItems)
+}
+
+const handleCheck = (id) => {
+  const listItems = items.map((item)=> item.id === id ? {...item,checked: !item.checked}: item)
+  setItems(listItems)
+  
+}
+
+const handleDelete = (id) => {
+ const listItems = items.filter((item)=> item.id !== id)
+ setItems(listItems)
+ 
+}
+
+const handleSubmit = (e) => {
+   e.preventDefault()
+   if (!newItem) return
+   addItem(newItem)
+   console.log(newItem)
+   setNewItem('')
+   
+}
   
   return (
     <div className='App'>
-     <Header />
-     <Content />
-     <Footer />
+     <Header title="Grocery List"/>
+    
+     <AddItem 
+      newItem={newItem}
+      setNewItem={setNewItem}
+      handleSubmit={handleSubmit}
+     />
+
+     <SearchItem
+        search={search}
+        setSearch={setSearch}
+      />
+     
+     <main>
+     {isLoading && <p>Loading Items...</p>}
+     {fetchError && <p style={{color:"red"}}>{`Error: ${fetchError}`}</p>}
+     {!fetchError && !isLoading && <Content 
+      items={items.filter(item=>((item.item).toLowerCase()).includes
+      (search.toLowerCase()))}
+      handleCheck={handleCheck}
+      handleDelete={handleDelete}
+     />}
+     </main>
+     <Footer length={items.length}/>
     </div>
   )
 }
--- revision/src/Content.jsx
+++ revision/src/Content.jsx
@@ -1,56 +1,15 @@
-import React, { useState } from 'react'
-import { FaTrashAlt } from 'react-icons/fa'
-const Content = () => {
-    const [items,setItems] = useState([
-        {
-            id:1,
-            checked:true,
-            item:'One half pound bag coca-almonds'
-        },
-        {
-            id:2,
-            checked:false,
-            item:'Samosa'
-        },
-        {
-            id:3,
-            checked:false,
-            item:'Khajooor'
-        }
-    ])
+import React from 'react'
 
-    const handleCheck = (id) => {
-        const listItems = items.map((item)=> item.id === id ? {...item,checked: !item.checked}: item)
-        setItems(listItems)
-        localStorage.setItem('ShoppingList', JSON.stringify(listItems))
-    }
-   
-    const handleDelete = (id) => {
-       const listItems = items.filter((item)=> item.id !== id)
-       setItems(listItems)
-       localStorage.setItem('ShoppingList', JSON.stringify(listItems))
-    }
+import ItemList from './ItemList'
+const Content = ({items, handleCheck, handleDelete}) => {
+    
   return (
-    <main>
+    <>
     {items.length ? (
-       <ul>
-        {items.map((item)=>(
-            <li className='item' key = {item.id}>
-                <input type="checkbox"
-                onChange={()=> handleCheck(item.id)}
-                checked={item.checked} />
-                <label
-                   style={(item.checked) ? { textDecoration:'line-through'}:null}
-                   onDoubleClick={()=> handleCheck(item.id)}
-                >{item.item}</label>
-               <FaTrashAlt 
-                onClick={()=>handleDelete(item.id)} 
-                role = "button"
-                tabIndex="0"
-               />
-            </li>
-        ))}
-       </ul>
+       <ItemList 
+         items = {items}
+         handleCheck={handleCheck}
+         handleDelete={handleDelete} />
        ) : (
         <p style={{ marginTop: '2rem'}}>
         <i>
@@ -58,7 +17,7 @@ const Content = () => {
         </i>
         </p>
        )} 
-    </main>
+    </>
   )
 }
 
